fd600803a4625f732957239f5e61d5ddb0e0e318
sunilbudhathoki01 <ashishbudhathoki952@gmail.com>
2025-12-13 16:15:07 +0545

    project setup for frontend and backend

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1 class="text-4xl font-bold text-red-600">app</h1>
+    </div>
+  );
+};
+
+export default App;
