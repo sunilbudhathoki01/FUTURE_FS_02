92db0d3eb6c65193db67d913db9f8dffeb7a2c1d
sunilbudhathoki01 <ashishbudhathoki952@gmail.com>
2025-12-13 19:11:00 +0545

    project successfully running

--- server/server.js
+++ server/server.js
@@ -1,3 +1,39 @@
 import express from "express";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.listen(PORT, () => console.log(`server running on ${PORT}`));
+
+// connect a database
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("mongodb successfully connected");
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+connectDb();
