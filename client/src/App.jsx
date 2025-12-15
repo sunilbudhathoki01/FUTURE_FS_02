2f37b9149a13a2c3118accf0e69cf5032f3e4ef4
sunilbudhathoki01 <ashishbudhathoki952@gmail.com>
2025-12-15 07:14:49 +0545

    routing succesfully tested

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,26 @@
 import React from "react";
-import "./index.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import AuthLogin from "./pages/auth/login";
+import AuthRegister from "./pages/auth/register";
+import AuthLayout from "./components/auth/Layout.jsx";
 
 const App = () => {
   return (
-    <div>
-      <h1 class="text-4xl font-bold text-red-600">app</h1>
-    </div>
+    <Routes>
+      {/* Redirect root "/" to "/auth" */}
+      <Route path="/" element={<Navigate to="/auth" />} />
+
+      {/* Auth routes */}
+      <Route path="/auth" element={<AuthLayout />}>
+        <Route index element={<AuthLogin />} /> {/* /auth */}
+        <Route path="login" element={<AuthLogin />} /> {/* /auth/login */}
+        <Route path="register" element={<AuthRegister />} />{" "}
+        {/* /auth/register */}
+      </Route>
+
+      {/* Optional: catch-all 404 page */}
+      <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+    </Routes>
   );
 };
 
