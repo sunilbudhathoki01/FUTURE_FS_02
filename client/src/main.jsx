3d25add9087b856f76d93628e708d27aa39179d1
sunilbudhathoki01 <ashishbudhathoki952@gmail.com>
2025-12-15 07:11:55 +0545

    main.jsx setup completed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
-import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+    <Provider store={store} />
     <App />
   </BrowserRouter>
 );
