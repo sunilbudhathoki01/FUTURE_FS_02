5523fe401b0a16b7d8bf481eff483b6f3ea20a9f
sunilbudhathoki01 <ashishbudhathoki952@gmail.com>
2025-12-15 07:18:33 +0545

    pages design init

---
+++ client/src/pages/auth/login.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AuthLogin = () => {
+  return <div className=" text-blue-800">login</div>;
+};
+
+export default AuthLogin;
---
+++ client/src/pages/auth/register.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AuthRegister = () => {
+  return (
+    <div>
+      <div>Regitser page</div>
+    </div>
+  );
+};
+
+export default AuthRegister;
