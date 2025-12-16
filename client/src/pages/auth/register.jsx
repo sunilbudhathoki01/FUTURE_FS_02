d7dd69a1dfb7ef744470e040e028e665fb73b73b
sunilbudhathoki01 <ashishbudhathoki952@gmail.com>
2025-12-16 22:09:55 +0545

    feat:register page succussfully complete with auth

--- client/src/pages/auth/register.jsx
+++ client/src/pages/auth/register.jsx
@@ -1,24 +1,49 @@
-import CommonForm from "@/components/common/Form";
+import CommonForm from "@/components/common/form";
+import { useToast } from "@/components/ui/use-toast";
 import { registerFormControls } from "@/config";
-import React, { useState } from "react";
-import { Link } from "react-router";
+import { registerUser } from "@/store/auth-slice";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 
 const initialState = {
   userName: "",
   email: "",
   password: "",
 };
-const AuthRegister = () => {
-  const [formdata, setFormData] = useState(initialState);
-  function onSubmit() {}
+
+function AuthRegister() {
+  const [formData, setFormData] = useState(initialState);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { toast } = useToast();
+
+  function onSubmit(event) {
+    event.preventDefault();
+    dispatch(registerUser(formData)).then((data) => {
+      if (data?.payload?.success) {
+        toast({
+          title: data?.payload?.message,
+        });
+        navigate("/auth/login");
+      } else {
+        toast({
+          title: data?.payload?.message,
+          variant: "destructive",
+        });
+      }
+    });
+  }
+
+  console.log(formData);
 
   return (
     <div className="mx-auto w-full max-w-md space-y-6">
-      <div className="text-center ">
+      <div className="text-center">
         <h1 className="text-3xl font-bold tracking-tight text-foreground">
-          create new account
+          Create new account
         </h1>
-        <p>
+        <p className="mt-2">
           Already have an account
           <Link
             className="font-medium ml-2 text-primary hover:underline"
@@ -31,12 +56,12 @@ const AuthRegister = () => {
       <CommonForm
         formControls={registerFormControls}
         buttonText={"Sign Up"}
-        formData={formdata}
+        formData={formData}
         setFormData={setFormData}
         onSubmit={onSubmit}
       />
     </div>
   );
-};
+}
 
 export default AuthRegister;
