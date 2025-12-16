b3aa23a9ace301ce3dd233a2a766609949cecbe7
sunilbudhathoki01 <ashishbudhathoki952@gmail.com>
2025-12-16 08:51:15 +0545

    adding auth controller for login logout and register

---
+++ server/controllers/auth/auth-controller.js
@@ -0,0 +1,38 @@
+import bcrypt from "bcryptjs";
+import User from "../../models/User.js";
+// register
+export const registerUser = async (req, res) => {
+  const { userName, email, password } = req.body;
+  try {
+    const hashPassword = await bcrypt.hash(password, 12);
+    const newUser = new User({
+      userName,
+      email,
+      password: hashPassword,
+    });
+    await newUser.save();
+    res.status(201).json({
+      success: true,
+      message: "Registration successfull",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "some error occured",
+    });
+  }
+};
+
+// login
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "some error occured",
+    });
+  }
+};
