e1e692921f7a01d5d9d80f2cb4744b766d9282f1
sunilbudhathoki01 <ashishbudhathoki952@gmail.com>
2025-12-16 07:30:31 +0545

    register page modified

--- client/src/pages/auth/register.jsx
+++ client/src/pages/auth/register.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import CommonForm from "@/components/common/Form";
+import { registerFormControls } from "@/config";
+import React, { useState } from "react";
+import { Link } from "react-router";
 
+const initialState = {
+  userName: "",
+  email: "",
+  password: "",
+};
 const AuthRegister = () => {
+  const [formdata, setFormData] = useState(initialState);
+  function onSubmit() {}
+
   return (
-    <div>
-      <div>Regitser page</div>
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center ">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+          create new account
+        </h1>
+        <p>
+          Already have an account
+          <Link
+            className="font-medium ml-2 text-primary hover:underline"
+            to="/auth/login"
+          >
+            Login
+          </Link>
+        </p>
+      </div>
+      <CommonForm
+        formControls={registerFormControls}
+        buttonText={"Sign Up"}
+        formData={formdata}
+        setFormData={setFormData}
+        onSubmit={onSubmit}
+      />
     </div>
   );
 };
