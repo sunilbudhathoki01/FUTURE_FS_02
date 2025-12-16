f3d804026d8271961544f34c70041c0abcf1f794
sunilbudhathoki01 <ashishbudhathoki952@gmail.com>
2025-12-16 07:27:04 +0545

    redirect pages for unauthorised access

---
+++ client/src/pages/unauth-page/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UnAuthPage = () => {
+  return (
+    <div>
+      <h1>you have to no access to view this page</h1>
+    </div>
+  );
+};
+
+export default UnAuthPage;
