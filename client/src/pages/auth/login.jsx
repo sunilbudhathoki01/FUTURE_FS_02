74e98915511c31fd00aafbe0cb7f0aadac33105c
sunilbudhathoki01 <ashishbudhathoki952@gmail.com>
2025-12-16 07:31:05 +0545

    login page modified

--- client/src/pages/auth/login.jsx
+++ client/src/pages/auth/login.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import CommonForm from "@/components/common/Form";
+import { loginFormControls } from "@/config";
+import React, { useState } from "react";
+import { Link } from "react-router";
 
+const initialState = {
+  email: "",
+  password: "",
+};
 const AuthLogin = () => {
-  return <div className=" text-blue-800">login</div>;
+  const [formdata, setFormData] = useState(initialState);
+  function onSubmit() {}
+
+  return (
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center ">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+          Sign in to your account
+        </h1>
+        <p>
+          Don't have an account
+          <Link
+            className="font-medium ml-2 text-primary hover:underline"
+            to="/auth/register"
+          >
+            Register
+          </Link>
+        </p>
+      </div>
+      <CommonForm
+        formControls={loginFormControls}
+        buttonText={"Sign In"}
+        formData={formdata}
+        setFormData={setFormData}
+        onSubmit={onSubmit}
+      />
+    </div>
+  );
 };
 
 export default AuthLogin;
