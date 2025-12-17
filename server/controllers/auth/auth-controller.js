62a31478bdbbceedb30875d12870428d84292656
sunilbudhathoki01 <ashishbudhathoki952@gmail.com>
2025-12-17 21:14:35 +0545

    feat(login) login logic auth and api design for login completed

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -1,9 +1,16 @@
 import bcrypt from "bcryptjs";
 import User from "../../models/User.js";
+import jwt from "jsonwebtoken";
 // register
 export const registerUser = async (req, res) => {
   const { userName, email, password } = req.body;
   try {
+    const checkUser = await User.findOne({ email });
+    if (checkUser)
+      return res.json({
+        success: false,
+        message: "same user with this email exist",
+      });
     const hashPassword = await bcrypt.hash(password, 12);
     const newUser = new User({
       userName,
@@ -28,6 +35,38 @@ export const registerUser = async (req, res) => {
 export const loginUser = async (req, res) => {
   const { email, password } = req.body;
   try {
+    const checkUser = await User.findOne({ email });
+    if (!checkUser) {
+      return res.json({
+        success: false,
+        message: "user does not exists! please register first",
+      });
+    }
+    const checkPasswordMatch = await bcrypt.compare(
+      password,
+      checkUser.password
+    );
+    if (!checkPasswordMatch) {
+      return res.json({ success: false, message: "password does not match" });
+    }
+    const token = jwt.sign(
+      {
+        id: checkUser._id,
+        role: checkUser.role,
+        email: checkUser.email,
+      },
+      "CLIENT_SECRET_KEY",
+      { expiresIn: "60mins" }
+    );
+    res.cookie("token", token, { httpOnly: true, secure: false }).json({
+      success: true,
+      message: "logged in successfully",
+      user: {
+        email: checkUser.email,
+        role: checkUser.role,
+        id: checkUser._id,
+      },
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({
