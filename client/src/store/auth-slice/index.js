6bb3f0d5c1b0412fd5d748c183ba070d4b62e9b9
sunilbudhathoki01 <ashishbudhathoki952@gmail.com>
2025-12-17 20:37:04 +0545

    feat(auth)add Redux Toolkit slice with async thunks for user registration and login

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -6,6 +6,7 @@ const initialState = {
   isLoading: false,
   user: null,
 };
+// for registration
 export const registerUser = createAsyncThunk(
   "/auth/register",
   async (FormData) => {
@@ -19,6 +20,17 @@ export const registerUser = createAsyncThunk(
     return response.data;
   }
 );
+// for login
+export const loginUser = createAsyncThunk("/auth/login", async (FormData) => {
+  const response = await axios.post(
+    "http://localhost:4000/api/auth/login",
+    FormData,
+    {
+      withCredentials: true,
+    }
+  );
+  return response.data;
+});
 const authSlice = createSlice({
   name: "auth",
   initialState,
@@ -27,6 +39,7 @@ const authSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder
+      // for registration
       .addCase(registerUser.pending, (state) => {
         state.isLoading = true;
       })
@@ -36,6 +49,20 @@ const authSlice = createSlice({
         state.isAuthenticated = false;
       })
       .addCase(registerUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isAuthenticated = false;
+      })
+      // for login
+      .addCase(loginUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = action.payload;
+        state.isAuthenticated = true;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
         state.isLoading = false;
         state.user = null;
         state.isAuthenticated = false;
