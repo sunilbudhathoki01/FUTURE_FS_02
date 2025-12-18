894311e2f0e9cac0d5b00e73f2e9ca8a7c494dec
sunilbudhathoki01 <ashishbudhathoki952@gmail.com>
2025-12-18 21:21:20 +0545

    feat:admin page design init

--- client/src/components/admin-view/Header.jsx
+++ client/src/components/admin-view/Header.jsx
@@ -1,7 +1,22 @@
+import { AlignJustify, LogOut } from "lucide-react";
 import React from "react";
+import { Button } from "../ui/button";
 
 const AdminHeader = () => {
-  return <div>admin header</div>;
+  return (
+    <header className="flex items-center justify-between px-4 py-3">
+      <Button className="lg:hidden sm:block">
+        <AlignJustify />
+        <span className="sr-only">Toggle menu</span>
+      </Button>
+      <div className="flex flex-1 justify-end">
+        <Button className="inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-medium shadow">
+          <LogOut />
+          Logout
+        </Button>
+      </div>
+    </header>
+  );
 };
 
 export default AdminHeader;
