11683ce6ca460d2cec1a6227c7b253ef2c6b8d9a
sunilbudhathoki01 <ashishbudhathoki952@gmail.com>
2025-12-19 08:20:41 +0545

    page:layout and admin logic for admin dashboard/admin-view

--- client/src/components/admin-view/SideBar.jsx
+++ client/src/components/admin-view/SideBar.jsx
@@ -4,8 +4,10 @@ import {
   LayoutDashboard,
   ShoppingBasket,
 } from "lucide-react";
-import React, { Fragment } from "react";
-import { useNavigate } from "react-router";
+import { Fragment } from "react";
+import { useNavigate } from "react-router-dom";
+import { Sheet, SheetContent, SheetHeader, SheetTitle } from "../ui/sheet";
+
 const adminSidebarMenuItems = [
   {
     id: "dashboard",
@@ -49,14 +51,28 @@ function MenuItems({ setOpen }) {
   );
 }
 
-const AdminSideBar = () => {
+function AdminSideBar({ open, setOpen }) {
   const navigate = useNavigate();
+
   return (
     <Fragment>
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetContent side="left" className="w-64">
+          <div className="flex flex-col h-full">
+            <SheetHeader className="border-b">
+              <SheetTitle className="flex gap-2 mt-5 mb-5">
+                <ChartNoAxesCombined size={30} />
+                <h1 className="text-2xl font-extrabold">Admin Panel</h1>
+              </SheetTitle>
+            </SheetHeader>
+            <MenuItems setOpen={setOpen} />
+          </div>
+        </SheetContent>
+      </Sheet>
       <aside className="hidden w-64 flex-col border-r bg-background p-6 lg:flex">
         <div
           onClick={() => navigate("/admin/dashboard")}
-          className="flex  cursor-pointer items-center gap-2"
+          className="flex cursor-pointer items-center gap-2"
         >
           <ChartNoAxesCombined size={30} />
           <h1 className="text-2xl font-extrabold">Admin Panel</h1>
@@ -65,6 +81,6 @@ const AdminSideBar = () => {
       </aside>
     </Fragment>
   );
-};
+}
 
 export default AdminSideBar;
