52c7060d3bb1575a9e99a01843619ba49f85412b
sunilbudhathoki01 <ashishbudhathoki952@gmail.com>
2025-12-23 22:17:40 +0545

    feat:added routes for cart

---
+++ server/routes/shop/cart-routes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import {
+  addToCart,
+  deleteCartItem,
+  fetchCartItems,
+  updateCartItemQty,
+} from "../../controllers/shop/cart-controller.js";
+const router = express.Router();
+router.post("/add", addToCart);
+router.get("/get/:userId", fetchCartItems);
+router.put("/update-cart", updateCartItemQty);
+router.delete("/delete/:userId/:productId", deleteCartItem);
+
+export default router;
