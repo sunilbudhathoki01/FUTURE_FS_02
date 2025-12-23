75e0fe7b4c3ae6928a2c1cdbfb0ef10ee5142690
sunilbudhathoki01 <ashishbudhathoki952@gmail.com>
2025-12-23 20:19:21 +0545

    typo error :error and mismatch between the routes

---
+++ server/routes/shop/productsRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import {
+  getFilteredProducts,
+  getProductDetails,
+} from "../../controllers/shop/productsController.js";
+const router = express.Router();
+router.get("/get", getFilteredProducts);
+router.get("/get/:id", getProductDetails);
+
+export default router;
